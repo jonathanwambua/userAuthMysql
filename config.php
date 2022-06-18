5bef05d91f321b54c48d66bab0b41533de218bae
Jonathan Wambua <jonathanwambua10219@gmail.com>
2022-06-18 08:08:19 +0300

    register, login, reset and delete fxn added

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ function db() {
     $host = "127.0.0.1";
     $user = "root";
     $db = "zuriphp";
-    $password = "Jonathan10219";
+    $password = "";
     $conn = mysqli_connect($host, $user, $password, $db);
     if(!$conn){
         echo "<script> alert('Error connecting to the database') </script>";
--- php/userauth.php
+++ php/userauth.php
@@ -1,5 +1,6 @@
 <?php
 
+session_start();
 require_once "../config.php";
 
 //register users
@@ -7,27 +8,67 @@ function registerUser($fullnames, $email, $password, $gender, $country){
     //create a connection variable using the db function in config.php
     $conn = db();
    //check if user with this email already exist in the database
+    $email_check = mysqli_query($conn, "SELECT * FROM students WHERE email = '$email'");
+    if(mysqli_num_rows($email_check) > 0){
+        echo "<script> alert('User with this email already exist') </script>";
+        
+    }else{
+        //insert the user details into the database
+        $query = "INSERT INTO students(`full_names`, `country`, `email`, `gender`, `password`) VALUES 
+            ('$fullnames', '$country', '$email', '$gender', '$password')";
+        if(mysqli_query($conn, $query)){
+            echo "<script> alert('User Successfully registered') </script>";
+            var_dump(mysqli_query($conn, $query));
+        }else{
+            echo "<script> alert('Error') </script>";
+        };
+        
+    }
+    // header("Location: ../forms/register.html");
 }
-
 
 //login users
 function loginUser($email, $password){
     //create a connection variable using the db function in config.php
     $conn = db();
 
-    echo "<h1 style='color: red'> LOG ME IN (IMPLEMENT ME) </h1>";
+    // echo "<h1 style='color: red'> LOG ME IN (IMPLEMENT ME) </h1>";
     //open connection to the database and check if username exist in the database
+     $email_check = mysqli_query($conn, "SELECT * FROM students WHERE email = '$email'");
     //if it does, check if the password is the same with what is given
+    $pass = null;
+    if(mysqli_num_rows($email_check) > 0){
+        while ($row = mysqli_fetch_assoc($email_check)) {
+            $pass = $row['password'];
+        }
+    }
     //if true then set user session for the user and redirect to the dasbboard
+    if($pass == $password){
+        $_SESSION['username'] = $email;
+        header("Location: ../dashboard.php");
+    }else{
+        echo "<script> alert('Password and Username do not match') </script>";
+        header("Location: ../forms/login.html");
+    }
 }
 
 
 function resetPassword($email, $password){
     //create a connection variable using the db function in config.php
     $conn = db();
-    echo "<h1 style='color: red'>RESET YOUR PASSWORD (IMPLEMENT ME)</h1>";
+    // echo "<h1 style='color: red'>RESET YOUR PASSWORD (IMPLEMENT ME)</h1>";
     //open connection to the database and check if username exist in the database
+    $email_check = mysqli_query($conn, "SELECT * FROM students WHERE email = '$email'");
     //if it does, replace the password with $password given
+    if(mysqli_num_rows($email_check) > 0){
+        $sql = "UPDATE students SET `password` = '$password' WHERE `email` = '$email'";
+        if(mysqli_query($conn, $sql)){
+            echo "<script> alert('Password Change Successfully') </script>";
+            header("Location: ../forms/login.html");
+        }
+    }else{
+        echo "<script> alert('User does not exist') </script>";
+    };
 }
 
 function getusers(){
@@ -65,4 +106,9 @@ function getusers(){
  function deleteaccount($id){
      $conn = db();
      //delete user with the given id from the database
+     $sql = "DELETE FROM students WHERE `id` = '$id'";
+     if(mysqli_query($conn, $sql)){
+        header("location: action.php?all=");
+        echo "<script> alert('User Deleted Successfully') </script>";
+     };
  }
